f56ee31e5117f2277f67b61a71f9609de9aa3ca1
Cristiano <britonano@gmail.com>
2026-02-09 10:46:19 -0300

    feat(01 criando objetos ): criada a classe usuario e seus objeto adicionada as msg de doc

---
+++ Usuarios.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Classe responsável por persistir dados dos usuarios salvos em arquivo de texto.
+ * Utilizada para cadastro e listagem simples, ideal para estudos e protótipos sem banco de dados.
+ */
+
+class Usuarios 
+{
+    /** @var caminho do arquivo onde os nome são armazenados */
+    private string $arquivo = 'dados.txt';
+
+    /**
+     * salva um novo nome no arquivo
+     * Adiciona nome ao final do arquivo
+     * @param string $nome o nome do usuario a ser salvo
+     * @return void 
+     */
+    public function salvar(string $nome): void
+    {
+      file_put_contents($this->arquivo, $nome . PHP_EOL, FILE_APPEND);
+    }
+
+
+    /**
+     * Retorna a lista de todos os usuarios cadastrados.
+     * Lê o arquivo linha por linha e retorna como array.
+     * Cada elemento do array é uma linha do arquivo (inclui possíveis \n se não filtrado).
+     * 
+     * @return array Lista de nomes (strings) 
+     */
+    public function listar(): array
+    {
+        if(! file_exists($this->arquivo))
+        {
+          echo 'nao encontrado';
+          return [];
+        }
+        
+        return file($this->arquivo);
+
+    }
+}
+
+?>
--- salvar.php
+++ salvar.php
@@ -1,16 +1,18 @@
 <?php
-$nome = $_POST['nome'] ?? '';                                   // sanitização dos dados evita erro
+require "Usuarios.php";
 
-if($nome === '')                                                // validação
+$nome = $_POST['nome'] ?? '';                    
+
+if($nome === '')                                    
 {
   echo "Nome obrigatorio";
   exit;
 }
 
-$arquivo = 'dados.txt';                                         // cria o arquivo de texto
-file_put_contents($arquivo, $nome . PHP_EOL, FILE_APPEND);      // escreve os nomes nesse arquivo
+$usuario =  new Usuarios();
+$usuario->salvar($nome);
 
-header('Location: listar.php');                                 // redireciona para o listar
+header('Location: listar.php');                                 
 exit;
 
 ?>
